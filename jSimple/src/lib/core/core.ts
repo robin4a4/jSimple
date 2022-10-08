92aac804294974d2c0b8ec1855016ad2e78dbfcf
robin marillia <robin4a4@gmail.com>
2022-10-08 17:46:42 +0200

    add first core functions

---
+++ jSimple/src/lib/core/core.ts
@@ -0,0 +1,76 @@
+const select = <TElement extends HTMLElement>(selector: string) =>
+  document.querySelector(selector) as TElement | null;
+
+const selectAll = <TElement extends HTMLElement>(selector: string) =>
+  [...document.querySelectorAll(selector)] as Array<TElement> | null;
+
+const create = <TCollection extends HTMLCollection>(htmlString: string) => {
+  const template = document.createElement("template");
+  template.innerHTML = htmlString.trim();
+  return template.content.children as TCollection;
+};
+
+declare global {
+  interface Array<T> {
+    first: () => Node;
+    last: () => Node;
+  }
+  interface NodeList {
+    first: () => Node;
+    last: () => Node;
+  }
+  interface HTMLElement {
+    addClass: (classes: string) => HTMLElement;
+    removeClass: (classes: string) => HTMLElement;
+    toggleClass: (classes: string) => HTMLElement;
+    html: (html: string) => HTMLElement;
+    empty: () => HTMLElement;
+  }
+}
+
+Array.prototype.first = function () {
+  return this[0];
+};
+
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
+
+NodeList.prototype.first = function () {
+  return [...this][0];
+};
+
+NodeList.prototype.last = function () {
+  return [...this][this.length - 1];
+};
+
+HTMLElement.prototype.addClass = function (classes: string) {
+  this.classList.add(classes);
+  return this;
+};
+
+HTMLElement.prototype.removeClass = function (classes: string) {
+  this.classList.remove(classes);
+  return this;
+};
+
+HTMLElement.prototype.toggleClass = function (classes: string) {
+  this.classList.toggle(classes);
+  return this;
+};
+
+HTMLElement.prototype.html = function (html: string) {
+  this.innerHTML = html;
+  return this;
+};
+
+HTMLElement.prototype.empty = function () {
+  this.innerHTML = "";
+  return this;
+};
+
+export const $ = {
+  select,
+  selectAll,
+  create,
+};
