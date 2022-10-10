483aeb659725c82202a8136fb11e45a2bd5adcfb
Robin Marillia <robinmarillia@macstudoderobin.home>
2022-10-11 00:16:58 +0200

    add workspaces and each shortcut

---
+++ docs/src/main.ts
@@ -0,0 +1,40 @@
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { setupCounter } from "./counter";
+import $ from "jsimple";
+
+type TObj = {
+  a: string;
+  b: string;
+};
+
+const obj: TObj = {
+  a: "test",
+  b: "pip",
+};
+
+$.each<TObj>(obj, (key, value) => {
+  console.log(key, value);
+});
+
+$.select<HTMLDivElement>("#app")!.html(
+  `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+);
+
+setupCounter($.select<HTMLButtonElement>("#counter")!);
