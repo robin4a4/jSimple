4bb78fb7d5ad30e46304db8e6a7b76027f94ea4a
Robin Marillia <robinmarillia@macstudoderobin.home>
2022-10-15 18:09:55 +0200

    add readme

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,38 +1,21 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import { setupCounter } from "./counter";
 import $ from "jsimple-core";
 
-$.select<TDiv>("#app")!.html(
-  `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-);
-
-setupCounter($.select<TButton>("#counter")!);
-const btn = $.select<TButton>("#btn");
-
-const [count, setCount] = $.signal(1);
-const doubleCount = () => count() * 2;
+const [isOpen, setIsOpen] = $.signal(false);
 
 $.effect(() => {
-  btn.html(doubleCount());
-});
+  const app = $.select("#app");
+  const btn = $.select("#btn");
 
-btn.onClick(() => {
-  setCount(count() + 1);
+  app.html(`
+  <div>
+    <button id="btn" type="button">open</button>
+    <div class="card" style='display: ${isOpen() ? "block" : "none"}'>
+      Lorem ipsum
+    </div>
+  </div>
+`);
+
+  btn.onClick(() => {
+    setIsOpen(!isOpen());
+  });
 });
